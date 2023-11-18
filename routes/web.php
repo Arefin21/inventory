6a8e285ab4853f1728a65abccee16521d69e461a
Arefin21 <arefinshagor18@gmail.com>
2023-11-18 13:53:40 +0600

    invoice&invProduct backendDone

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\TokenVerificationMiddleware;
@@ -30,6 +31,8 @@ Route::get('/userProfile', [UserController::class, 'profilePage'])->middleware([
 Route::get('/categoryPage', [CategoryController::class, 'CategoryPage'])->middleware([TokenVerificationMiddleware::class]);
 Route::get('/customerPage', [CustomerController::class, 'CustomerPage'])->middleware([TokenVerificationMiddleware::class]);
 Route::get('/productPage', [ProductController::class, 'ProductPage'])->middleware([TokenVerificationMiddleware::class]);
+Route::get('/invoicePage', [InvoiceController::class, 'InvoicePage'])->middleware([TokenVerificationMiddleware::class]);
+Route::get('/salePage', [InvoiceController::class, 'SalePage'])->middleware([TokenVerificationMiddleware::class]);
 
 Route::get('/user-profile', [UserController::class, 'userProfile'])->middleware([TokenVerificationMiddleware::class]);
 Route::post('/user-update', [UserController::class, 'updateProfile'])->middleware([TokenVerificationMiddleware::class]);
@@ -51,8 +54,15 @@ Route::post('/update-customer', [CustomerController::class, 'CustomerUpdate'])->
 Route::post('/customer-by-id', [CustomerController::class, 'CustomerByID'])->middleware(TokenVerificationMiddleware::class);
 Route::post('/customer-by-id', [CustomerController::class, 'CustomerByID'])->middleware(TokenVerificationMiddleware::class);
 
+//Product Api
 Route::post('/create-product', [ProductController::class, 'CreateProduct'])->middleware(TokenVerificationMiddleware::class);
 Route::post('/delete-product', [ProductController::class, 'DeleteProduct'])->middleware(TokenVerificationMiddleware::class);
 Route::post('/product-by-id', [ProductController::class, 'ProductByID'])->middleware(TokenVerificationMiddleware::class);
 Route::get('/list-product', [ProductController::class, 'ProductList'])->middleware(TokenVerificationMiddleware::class);
 Route::post('/update-product', [ProductController::class, 'ProductUpdate'])->middleware(TokenVerificationMiddleware::class);
+
+//Invoice
+Route::post('/invoice-create', [InvoiceController::class, 'invoiceCreate'])->middleware(TokenVerificationMiddleware::class);
+Route::get('/invoice-select', [InvoiceController::class, 'invoiceSelect'])->middleware(TokenVerificationMiddleware::class);
+Route::post('/invoice-details', [InvoiceController::class, 'InvoiceDetails'])->middleware(TokenVerificationMiddleware::class);
+Route::post('/invoice-delete', [InvoiceController::class, 'invoiceDelete'])->middleware(TokenVerificationMiddleware::class);
